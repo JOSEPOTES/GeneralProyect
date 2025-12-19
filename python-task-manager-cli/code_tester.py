f0d6363a94b432586c6f3756066f7fd0248a17b0
Jose_Potes <josepotes200@gmail.com>
2025-12-19 12:45:54 -0500

    Issue Fix
    he actualizado el callback a la funcion run, ahora si hay un KeyboardInterrupt vuelve al run().
    issue solucionado

--- python-task-manager-cli/code_tester.py
+++ python-task-manager-cli/code_tester.py
@@ -1,5 +1,38 @@
-number = input("Escribe un numero: ")
-if number.isdigit():
-    print("Numero")
-else:
-    print("No es un numero: ")
+    file_name = input("Nombre del archivo: ").lower()
+    # almacena el resultado de la busqueda del archivo
+    try:
+        # verifica si el archivo escrito en 85line existe.
+        if not (os.path.exists(f"{file_name}.json")):
+            question_create_file = input(
+                "El archivo no existe, 1) Crear nuevo, 2) Salir: "
+            )
+            try:
+                while not question_create_file in ["1", "2"]:
+                    raise ValueError("La opción no es valida")
+            except ValueError as no_valid:
+                print(no_valid)
+            if question_create_file == "1":
+                create_task()
+            elif question_create_file == "2":
+                return False
+        task_title = input("Nombre de la tarea: ").lower()
+        with open(f"{file_name}.json", "r", encoding="utf-8") as file_read:
+            data_json = json.load(file_read)
+            task_dict = {task[task_title] for task in data_json}
+            print(task_dict)
+    except:
+        print("Leyendo")
+        
+        
+def start_program():
+    while True:
+        try:
+
+            if 1 <= opcion <= 5:
+                return opcion
+            else:
+                print("[!] Por favor, elige un numero del 1 al 5")
+        except ValueError:
+            print("Debes ingresar un numero entero")
+        except KeyboardInterrupt:
+            return False
--- python-task-manager-cli/main.py
+++ python-task-manager-cli/main.py
@@ -1,25 +1,7 @@
-def start_program():
-    while True:
-        try:
-            opcion = int(
-                input(
-                    """
-            1: Crear tarea.
-            2: Listar tareas.
-            3: Actualizar tareas.
-            4: Eliminar tareas.
-            5: Salir.
-            Digita: """
-                )
-            )
-            if 1 <= opcion <= 5:
-                return opcion
-            else:
-                print("[!] Por favor, elige un numero del 1 al 5")
-        except ValueError:
-            print("Debes ingresar un numero entero")
-        except KeyboardInterrupt:
-            return False
+import json
+from typing import Any
+import os
+from pathlib import Path
 
 
 def question_result() -> str | None:
@@ -34,18 +16,17 @@ def question_result() -> str | None:
 
 
 def create_task():
+    file_name: str = input("Nombre del archivo: ").lower().strip().replace(" ", "_")
     task_list = []
     try:
         MAX_TASK = 10
         contador = 1
         while contador < MAX_TASK:
             print(f"Tarea: {contador} ")
-            title = input("Titulo: ").capitalize()
-            description = input("Description: ").capitalize()
+            title = input("Titulo: ").lower()
+            description = input("Description: ").lower()
             due_date = input("Fecha caducidad: ")
-            state = input(
-                "Escriba el estado(pendiente,terminada,cancelada): "
-            ).capitalize()
+            state = input("Escriba el estado(pendiente,terminada,cancelada): ").lower()
             task_list.append(
                 {
                     "title": title,
@@ -55,51 +36,98 @@ def create_task():
                     "state": ("undefined" if len(state) <= 0 else state),
                 }
             )
+            with open(f"{file_name}.json", "w", encoding="UTF-8") as file:
+                json.dump(task_list, file, indent=4)
             contador += 1
             if contador == 6:
                 add_more = question_result()
                 if add_more == "s":
+                    os.system("cls")
                     continue
                 elif add_more == "n":
+                    os.system("cls")
                     return task_list
                 else:
                     return False
         return task_list
     except KeyboardInterrupt:
-        return task_list
+        print("saliendo....")
+        return run()
 
 
+# FIXME
 def read_task():
-    print("Leyendo tarea")
+    file_name = input("Nombre del archivo: ").lower()
+    # almacena el resultado de la busqueda del archivo
+    try:
+        # verifica si el archivo escrito en 85line existe.
+        if not (os.path.exists(f"{file_name}.json")):
+            question_create_file = input(
+                "El archivo no existe, 1) Crear nuevo, 2) Salir: "
+            )
+            if question_create_file == "1":
+                create_task()
+            elif question_create_file == "2":
+                print("saliendo")
+                run()
+        task_title = input("Nombre de la tarea: ").lower()
+        with open(f"{file_name}.json", "r", encoding="utf-8") as file_read:
+            data_json = json.load(file_read)
+            task_dict = {task[task_title] for task in data_json}
+            print(task_dict)
+    except:
+        print("Leyendo")
 
 
 def update_task():
     print("Actualizar")
+    run()
 
 
 def delete_task():
     print("Eliminar")
+    run()
 
 
 def run():
     try:
-        start = start_program()
-        if start == 1:
-            print(create_task())
-        elif start == 2:
-            read_task()
-        elif start == 3:
-            update_task()
-        elif start == 4:
-            delete_task()
-        elif start == 5:
-            print("Saliendo...")
-        else:
-            raise ValueError("Terminando programa")
+        opcion = int(
+            input(
+                """
+            1: Crear tarea.
+            2: Listar tareas.
+            3: Actualizar tareas.
+            4: Eliminar tareas.
+            5: Salir.
+            Digita: """
+            )
+        )
+        while not (1 <= opcion <= 5):
+            print("Optición no valida")
+            opcion = int(
+                input(
+                    """
+                            1: Crear tarea.
+                            2: Listar tareas.
+                            3: Actualizar tareas.
+                            4: Eliminar tareas.
+                            5: Salir.
+                            Digita: """
+                )
+            )
+        match opcion:
+            case 1:
+                create_task()
+            case 2:
+                read_task()
+            case 3:
+                update_task()
+            case 4:
+                delete_task()
+            case 5:
+                print("Terminando el programa")
     except KeyboardInterrupt:
-        print("Terminando programa")
-    except ValueError as exit:
-        print(exit)
+        print("-> Programa terminado por el usuario.")
 
 
 if __name__ == "__main__":
